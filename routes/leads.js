923bee88932c67ec45f9bd5e274170783d6d46dd
Karunasree <karuna.r@westagilelabs.com>
2024-08-07 11:37:39 +0530

    leads list

--- routes/leads.js
+++ routes/leads.js
@@ -3,7 +3,7 @@ var router = express.Router();
 const leadsController = require("../controllers/leads");
 
 router.post("/", leadsController.createLead);
-router.get("/list", leadsController.leadsList);
+router.get("/", leadsController.leadsList);
 router.get("/:email", leadsController.getLead);
 router.put("/:email", leadsController.editLead);
 router.delete("/:email", leadsController.deleteLead);
